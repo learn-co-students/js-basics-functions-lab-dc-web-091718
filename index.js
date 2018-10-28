0b2a21dd82f8ebee9120594675eefa1a77be27ce
DeCode2018 <decode2018@gmail.com>
2018-10-27 23:51:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+  blocks = Math.abs(pickup-42);
+  return blocks;
+}
+
+function distanceFromHqInFeet(pickup){
+  feet = Math.abs((pickup-42))*264;
+  return feet;
+}
+
+function distanceTravelledInFeet(pickup,dropoff){
+  totalFeet = Math.abs((pickup-dropoff))*264;
+  return totalFeet;
+}
+
+
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+    }
+    else if (distance > 400 && distance <= 2000) {
+    return .02*(distance - 400);
+    }
+    else if (distance > 2000 && distance < 2500){
+    return 25;
+    }
+    else {
+    return 'cannot travel that far';
+    }
+}
